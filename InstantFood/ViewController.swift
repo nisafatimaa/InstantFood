66a9067ea4dbb5d8b22208eb25a40d24521b1f4b
Nisa Fatima <nisafatima0000@gmail.com>
2024-04-15 13:41:29 +0500

    Created BezierPath in view controller

--- InstantFood/ViewController.swift
+++ InstantFood/ViewController.swift
@@ -9,11 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    var shape : CAShapeLayer?
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        shape?.removeFromSuperlayer()
+        drawBottomLayer()
+        
     }
 
+    func drawBottomLayer(){
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.fillColor = UIColor(named: "Brown")?.cgColor
+        
+        let path = UIBezierPath()
+        let rect = UIScreen.main.bounds
+        let width = rect.width
+        let height = rect.height
+    
+        path.move(to: CGPoint(x: 0, y: height))
+        path.addLine(to: CGPoint(x: width, y: height))
+        path.addLine(to: CGPoint(x: width, y: height - 300))
+        
+        path.addCurve(to: CGPoint(x: width - 195, y: height - 160),
+                      controlPoint1: CGPoint(x: width - 30, y: height - 174),
+                      controlPoint2: CGPoint(x: width - 190, y: height - 160))
+        path.addCurve(to: CGPoint(x: 0, y: height),
+                      controlPoint1: CGPoint(x: width - 220, y: height - 160 ),
+                      controlPoint2: CGPoint(x: width - 370, y: height - 150 ))
+        
+        path.close()
+        
+        shapeLayer.path = path.cgPath
+        self.view.layer.addSublayer(shapeLayer)
+        self.shape = shapeLayer
+        
+    }
 
 }
 
