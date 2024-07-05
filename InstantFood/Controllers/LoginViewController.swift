a8488bf1db81727ba441c65cd2dcf13a58f0b1d8
Nisa Fatima <nisafatima0000@gmail.com>
2024-07-05 20:48:27 +0500

    completed google and email login

--- InstantFood/Controllers/LoginViewController.swift
+++ InstantFood/Controllers/LoginViewController.swift
@@ -12,6 +12,12 @@ class LoginViewController: UIViewController {
     @IBOutlet var emailField: UITextField!
     @IBOutlet var passwordField: UITextField!
     @IBOutlet weak var registerSegment : UISegmentedControl!
+    @IBOutlet weak var loginButton : UIButton!
+    
+    private var isEmailValid = false
+    private var isPasswordValid = false
+    var loginManager = LoginManager()
+    var signupManager = SignupManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,25 +26,69 @@ class LoginViewController: UIViewController {
         registerSegment.setTitleTextAttributes([.foregroundColor : K.lightColor!], for: .selected)
         
         navigationItem.hidesBackButton = true
+        updateLoginButton()
     }
     
     @IBAction func registerSegment(_ sender: UISegmentedControl) {
+        
         guard let vc = storyboard?.instantiateViewController(withIdentifier: K.signupVCIdentifier) as? SignupViewController else { return }
          navigationController?.pushViewController(vc, animated: true)
 
     }
     
     @IBAction func loginClicked(_ sender: UIButton) {
+        
+        guard let email = emailField.text,
+              let password = passwordField.text else { return }
+        
+        validateInfo()
+        loginManager.loginWithEmail(self, email, password)
+        
     }
     
     @IBAction func googleLogin(_ sender: UIButton) {
+        loginManager.signupWithGoogle(self)
     }
     
     @IBAction func facebookLogin(_ sender: UIButton) {
+        
     }
     
     @IBAction func backpressed(_ sender: UIButton) {
         guard let vc = storyboard?.instantiateViewController(withIdentifier: K.signupVCIdentifier) as? SignupViewController else { return }
          navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    func updateLoginButton () {
+        loginButton.isEnabled = isEmailValid && isPasswordValid
+        loginButton.backgroundColor = loginButton.isEnabled ? K.primaryColor : K.primaryColor?.withAlphaComponent(0.7)
+    }
+    
+    func validateInfo() {
+        if isEmailValid == false {
+            AlertMessage.showAlertMessage("Invalid Email", "Please enter valid email", self)
+            
+        } else if isPasswordValid == false {
+            AlertMessage.showAlertMessage("Invalid Password", "Password must contain at least 8 letters", self)
+            
+        }
+    }
+}
+
+
+
+// MARK: - TextFieldDelegate
+extension LoginViewController : UITextFieldDelegate {
+    
+    func textFieldDidChangeSelection(_ textField: UITextField) {
+        
+        if textField == emailField {
+            isEmailValid = signupManager.isEmailValid(textField.text ?? "")
+            
+        } else if textField == passwordField {
+            isPasswordValid = signupManager.isPasswordValid(textField.text ?? "")
+        
+        }
+        updateLoginButton()
     }
 }
